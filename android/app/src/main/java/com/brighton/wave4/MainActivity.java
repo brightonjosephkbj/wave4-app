d4b358d493be674aaeb8159778ba4c6dd9960212
brightonjosephkbj <brightonjosephkbj@email.com>
2026-04-16 18:47:13 +0300

    Fix download opening browser

--- android/app/src/main/java/com/brighton/wave4/MainActivity.java
+++ android/app/src/main/java/com/brighton/wave4/MainActivity.java
@@ -1,5 +1,25 @@
-package com.brighton.wave4;
+package com.brightonjosephkbj.wave4app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.webkit.DownloadListener;
+import android.content.Intent;
+import android.net.Uri;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    bridge.getWebView().setDownloadListener(
+      new DownloadListener() {
+        public void onDownloadStart(String url, String userAgent,
+            String contentDisposition, String mimetype,
+            long contentLength) {
+          Intent intent = new Intent(Intent.ACTION_VIEW);
+          intent.setData(Uri.parse(url));
+          startActivity(intent);
+        }
+      }
+    );
+  }
+}
